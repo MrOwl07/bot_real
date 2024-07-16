e6e9a1abd08bff3340ee12995b1d70fc39c92fef
esteman500@gmail.com <esteman500@gmail.com>
2024-07-16 11:47:11 -0500

    memes

--- BOT_AL_Mando.py
+++ BOT_AL_Mando.py
@@ -1,12 +1,14 @@
 import discord
+import os
 from discord.ext import commands
 from bot_logic import gen_pass
+import random as r
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-
+token = "MTIzOTk4NTA5OTU4MDI0ODA4NA.GZ28rj.wd3y1ppa6vMLfxiqnzN0UpXMxGFH6wzaWyTdR8"
 @bot.event
 async def on_ready():
     print(f'Hemos iniciado sesión como {bot.user}')
@@ -15,9 +17,28 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send(f'Hola, soy un bot {bot.user}!')
 
-@bot.command()
-async def pasw(ctx):
-    await ctx.send(gen_pass(10))
+@bot.command(name = "Password")
+async def pasw(ctx,a):
+    nombre = str(a)
+    await ctx.send(gen_pass(20)+nombre)
+    
+@bot.command(name="suma")
+async def sumar(ctx,a,b):
+    response = int(a) + int(b)
+    await ctx.send(f"la suma es:{response}")
+    
+@bot.command(name = "meme")
+async def mem(ctx):
+    with open("Memes/juzgar.jpg","rb") as f:
+        picture = discord.File(f)
+    await ctx.send(file = picture)
+    
+@bot.command(name = "momos")
+async def momo(ctx):
+    img_mem = r.choice(os.listdir("Memes"))
+    with open(f"Memes/{img_mem}","rb") as f:
+        picture = discord.File(f)
+    await ctx.send(file = picture)
 
-bot.run("MTIzOTk4NTA5OTU4MDI0ODA4NA.GZ28rj.wd3y1ppa6vMLfxiqnzN0UpXMxGFH6wzaWyTdR8")
+bot.run(token)
 
