de69e8d3a6d1410b4a2ecaf52be596bd6b19d47c
esteman500@gmail.com <esteman500@gmail.com>
2024-07-16 13:09:15 -0500

    anime y patos

--- BOT_AL_Mando.py
+++ BOT_AL_Mando.py
@@ -3,12 +3,32 @@ import os
 from discord.ext import commands
 from bot_logic import gen_pass
 import random as r
-
+import requests
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 token = "MTIzOTk4NTA5OTU4MDI0ODA4NA.GZ28rj.wd3y1ppa6vMLfxiqnzN0UpXMxGFH6wzaWyTdR8"
+
+#funcion para la solicitud de api para imagenes de patos
+def get_duck_image():
+    url = 'https://random-d.uk/api/random'
+    res =  requests.get(url)
+    data = res.json()
+    return data['url']
+#funcion de consulta API am¿nime
+def get_anime_image(query):
+    url = ' https://kitsu.io/api/edge/anime'
+    params = {
+        'filter[text]' : query
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 @bot.event
 async def on_ready():
     print(f'Hemos iniciado sesión como {bot.user}')
@@ -40,6 +60,24 @@ async def momo(ctx):
         picture = discord.File(f)
     await ctx.send(file = picture)
 
+@bot.command(name = "patos")
+async def duck(ctx):
+    image_url = get_duck_image()
+    await ctx.send(image_url)
+
+@bot.command(name = "anime")
+async def anime(ctx, a):
+    query = a
+    anime_data = get_anime_image(query)
+
+    if anime_data:
+        for anime in anime_data:
+            
+            image_url = anime['attributes']['posterImage']['small']
+
+            await ctx.send(f"Image URL: {image_url}")
+    else:
+        await ctx.send("No se pudieron obtener datos de anime.")
 bot.run(token)
 
-#creacion
+
