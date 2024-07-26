f3349433f35ed7bac5e647126f8364ce7f649bd1
esteman500@gmail.com <esteman500@gmail.com>
2024-07-25 23:53:57 -0500

    cambio por seguridad

--- BOT_AL_Mando.py
+++ BOT_AL_Mando.py
@@ -5,11 +5,14 @@ from bot_logic import gen_pass
 import random as r
 import requests
 import comand_api as c
+from dotenv import load_dotenv
+
+load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-token = "INGRESE EL TOKEN"
+token = os.getenv("dt")
 
 
 @bot.event
@@ -19,37 +22,39 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hola, soy un bot {bot.user}!')
-
+    
+#comando para crear contraseña
 @bot.command(name = "Password")
 async def pasw(ctx,a,b):
     nombre = str(a)
     await ctx.send(gen_pass(b)+nombre)
-    
+
+#comando para generar una suma de dos digitos
 @bot.command(name="suma")
 async def sumar(ctx,a,b):
     response = int(a) + int(b)
     await ctx.send(f"la suma es:{response}")
 
-#Clese hoy
+#comando que rastrea una imagen dentro de una carpeta
 @bot.command(name = "meme")
 async def mem(ctx):
     with open("Memes/juzgar.jpg","rb") as f:
         picture = discord.File(f)
     await ctx.send(file = picture)
 
-#CLase de HOY
+#Comando para arrojar un meme aleatorio desde la carpeta local
 @bot.command(name = "momos")
 async def momo(ctx):
     img_mem = r.choice(os.listdir("Memes"))
     with open(f"Memes/{img_mem}","rb") as f:
         picture = discord.File(f)
     await ctx.send(file = picture)
-#CLase de HOY
+#Comando para activar API de img random de patos
 @bot.command(name = "patos")
 async def duck(ctx):
     image_url = c.get_duck_image()
     await ctx.send(image_url)
-#CLase de HOY
+#Comando para activar API de img de anime por busqueda con palabra clave
 @bot.command(name = "anime")
 async def anime(ctx, a):
     query = a
